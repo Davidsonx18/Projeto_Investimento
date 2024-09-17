3217409361a5d0fbd0017351be1367db0164f061
Davidsonx18 <gkiller454@gmail.com>
2024-09-16 23:45:46 -0300

    att 1.3

--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@-s&rg2=v!xrb$e&d8weklac871)ou1mueojfc7)j-@9^tdp@6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
